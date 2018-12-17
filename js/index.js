34c1e33ceafc802cc398f57d48f3a7c251150d6d
Kieran <45768489+wadforth@users.noreply.github.com>
2018-12-17 01:09:22 +0000

    v2.0 - Design Overhaul
    Website was merged into 1 for better design and easier accessability.
    
    +Toggable Profile Sidebar
    +Projects & Contact Same Page
    
    Current Known Bugs
    - If profile toggle is on, mobile design will be bugged.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,10 @@
-$(document).ready(function() {
-    $('[data-toggle="tooltip"]').tooltip();   
+//$(document).ready(function() {
+   // $('[data-toggle="tooltip"]').tooltip();   
+//});
+         
+
+$(document).ready(function () {
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
 });
-                                    
